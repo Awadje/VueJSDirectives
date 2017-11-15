a6c6a00f014d594fbd37d67c1297413da0a842df
Awadje <michaelawad85@gmail.com>
2017-11-15 20:16:48 +0100

    Created blinking directive

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color This</p>
+								<p v-local-highlight:background.delayed.blink="'red'">Color This 2</p>
             </div>
         </div>
     </div>
@@ -19,10 +20,43 @@
 </template>
 
 <script>
-    export default {
-    }
+	export default {
+		directives: {
+			'local-highlight': {
+				bind(el, binding, vnode) {
+					var delay = 0;
+					if (binding.modifiers['delayed']) {
+							delay = 3000;
+					}
+					if (binding.modifiers['blink']) {
+						let mainColor = binding.value;
+						let secondColor = 'blue';
+						let currentColor = mainColor;
+						setTimeout(() => {
+							setInterval(() => {
+								currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor
+								if (binding.arg == 'background') {
+									el.style.backgroundColor = currentColor;
+							} else {
+									el.style.color = binding.value;
+								}
+						}, 1000);
+					}, delay);
+				} else {
+					setTimeout(() => {
+						if (binding.arg == 'background') {
+								el.style.backgroundColor = currentColor;
+					} else {
+							el.style.color = currentColor;
+							}
+						}, delay);
+					}
+				}
+			}
+		}
+	}
 </script>
 
 <style>
-
+	
 </style>
